e2cc624afdc98e9f21bf2f18ddeb12a7edc9a577
fazle rabbi <frabby983@gmail.com>
2022-06-01 10:37:08 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let randomBtn = document.querySelector('.random');
+let imgSrc = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg','img/9.jpg','img/10.jpg','img/11.jpg','img/12.jpg','img/13.jpg',];
+let imgTag = document.querySelector('img');
+randomBtn.addEventListener("click", ()=>{
+ let randomNumber = Math.floor(Math.random()*13);
+ imgTag.src=imgSrc[randomNumber];
+// imgBox.style.backgroundSize='cover';
+})
